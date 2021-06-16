16639fa38e706686b944d6ffeda52e467fd49e2b
BlinkingBerry <berry.that.blinks@gmail.com>
2021-06-16 19:36:36 +0100

    can't remember what I changed exactly

--- more-commands.user.js
+++ more-commands.user.js
@@ -2,7 +2,10 @@
 // @name         More Commands
 // @description  Adds more commands, idk
 // @author       Blinking Berry
+// @version      banana
 // @namespace    http://bcmc.ga/authors/blinkingberry/
+// @updateURL    https://github.com/BlinkingBerry/more-commands/blob/master/more-commands.user.js
+// @downloadURL  https://github.com/BlinkingBerry/more-commands/blob/master/more-commands.user.js
 // @run-at       document-start
 // @grant        none
 // @include      /^https:\/\/boxcritters\.com\/play\/(index\.html)?([\?#].*)?$/
@@ -14,17 +17,18 @@
 (function () {
 	'use strict';
 	cardboard.register('moreCommands');
+
 	cardboard.on('loadScriptWorld', function (t) {
 		t.innerHTML = t.innerHTML.replace(
             /if\("gear"===i\)return this\.updateGear\(e\);/,
 			`
             if ("gear" === i)
 				return this.updateGear(e);
-            if ("add" === i && !this.player.gear.includes(e))
+            if ("add" === i)
 				return this.updateGear(this.player.gear.concat(e));
             if ("remove" === i || "rm" == i)
 				return this.updateGear(this.player.gear.filter(it => {return !e.includes(it)}));
-            `
+			`
 		);
 	});
 })();
